82485432270f29643547bbf11ad76b5f96743681
David van Beveren <admin@veganhacktivists.org>
2020-08-12 15:48:59 +0100

    Update map link

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
     </style>
 </head>
 <body style="margin:0px;padding:0px;overflow:hidden">
-    <iframe src="https://www.google.com/maps/d/u/0/viewer?hl=de&ll=38.56165186366597%2C21.865702589096458&z=4&mid=1riR6Rl0KTltNrzpfddesLauOwk0QnIs5" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+	<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1riR6Rl0KTltNrzpfddesLauOwk0QnIs5" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
 </body>
+</html>
