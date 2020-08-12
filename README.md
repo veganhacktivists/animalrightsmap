ed6043a80fb31ecabfffca394571fc510b842734
David van Beveren <admin@veganhacktivists.org>
2020-08-12 15:41:27 +0100

    Initial commit for project

--- README.md
+++
@@ -1,1 +0,0 @@
-# animalrightsmap
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <title>Animal Rights Activism Map - The largest map of animal rights organization groups!</title>
+    <link rel="shortcut icon" href="favicon.ico" />
+    <style>
+       html, body {
+           height: 100%;
+       }
+    </style>
+</head>
+<body style="margin:0px;padding:0px;overflow:hidden">
+    <iframe src="https://www.google.com/maps/d/u/0/viewer?hl=de&ll=38.56165186366597%2C21.865702589096458&z=4&mid=1riR6Rl0KTltNrzpfddesLauOwk0QnIs5" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+</body>
