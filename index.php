731821ed3d97204808c46b4e29d6c3600e26ea69
David van Beveren <admin@veganhacktivists.org>
2020-08-13 00:20:12 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     </style>
 </head>
 <body style="margin:0px;padding:0px;overflow:hidden">
-<iframe is="x-frame-bypass" src="https://www.google.com/maps/d/u/0/edit?hl=de&mid=1riR6Rl0KTltNrzpfddesLauOwk0QnIs5&ll=38.56165186366597%2C21.865702589096458&z=4" style="width:100%; height:100vh;" frameborder="0"></iframe>
+<iframe is="x-frame-bypass" src="https://www.google.com/maps/d/viewer?hl=de&mid=1riR6Rl0KTltNrzpfddesLauOwk0QnIs5&ll=38.56165186366597%2C21.865702589096458&z=4" style="width:100%; height:100vh;" frameborder="0"></iframe>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="module" src="https://unpkg.com/x-frame-bypass"></script>
 <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
