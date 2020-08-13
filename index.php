a7f298176a365b4e1415c251e0e863b8e018fe42
David van Beveren <admin@veganhacktivists.org>
2020-08-13 14:23:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ div.content {
 
 <div class="content" style="padding:0px;overflow:hidden">
 
-<iframe width="100%" height="300px" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/en/map/animal-rights-map_487135?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false"></iframe><p><a href="//umap.openstreetmap.fr/en/map/animal-rights-map_487135">See full screen</a></p>
+<iframe width="100%" height="300px" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/en/map/animal-rights-map_487135?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
 
 </div>
 
